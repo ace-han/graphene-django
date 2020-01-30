dfcdf53a9393a3f753ffcf0158165329e5402b5f
Ace <ace.jl.han@gmail.com>
2020-01-30 17:13:42 +0800

    add basic test index

--- examples/cookbook/cookbook/urls.py
+++ examples/cookbook/cookbook/urls.py
@@ -3,8 +3,16 @@ from django.contrib import admin
 
 from graphene_django.views import GraphQLView
 
+from django.http import HttpResponse
+
+
+def index(request):
+    print("hahaha")
+    return HttpResponse("ok")
+
 
 urlpatterns = [
     url(r"^admin/", admin.site.urls),
+    url(r"^index/", index),
     url(r"^graphql$", GraphQLView.as_view(graphiql=True)),
 ]

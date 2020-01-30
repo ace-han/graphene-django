b653607d65f64717633384d9720a30a890c3f83f
Ace <ace.jl.han@gmail.com>
2020-01-30 22:49:33 +0800

    simple mutation for Category Creation

--- examples/cookbook/cookbook/ingredients/schema.py
+++ examples/cookbook/cookbook/ingredients/schema.py
@@ -1,5 +1,5 @@
 from cookbook.ingredients.models import Category, Ingredient
-from graphene import Node, ObjectType
+from graphene import Boolean, Field, Node, Mutation, ObjectType, String
 from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django.types import DjangoObjectType
 
@@ -27,8 +27,60 @@ class IngredientNode(DjangoObjectType):
 
 
 class Queries(ObjectType):
-    category = Node.Field(CategoryNode)
+    category = Field(CategoryNode)
     all_categories = DjangoFilterConnectionField(CategoryNode)
 
-    ingredient = Node.Field(IngredientNode)
+    ingredient = Field(IngredientNode)
     all_ingredients = DjangoFilterConnectionField(IngredientNode)
+
+
+# simple mutation
+class CategoryCreate(Mutation):
+    """
+    mutation {
+        categoryCreate(name: "Condiment") {
+            created
+            category {
+                id
+                name
+            }
+        },
+        _debug {
+            sql {
+                rawSql,
+                sql,
+                vendor
+            }
+        }
+    }
+    """
+
+    # The class attributes define the response of the mutation
+    created = Boolean()
+    category = Field(CategoryNode)
+
+    class Arguments:
+        # The input arguments for this mutation
+        name = String(required=True)
+
+    @classmethod
+    def mutate(cls, root, info, **kwargs):
+        # some discussion not resolved at the time of writing
+        # refer to
+        # https://docs.graphene-python.org/en/latest/types/objecttypes/#resolver-parameters
+        # https://github.com/graphql-python/graphene/issues/810
+        # stick with classmethod for the time being
+        name = kwargs.get("name", "")
+        obj, created = Category.objects.get_or_create(name=name)
+        return cls(created=created, category=obj)
+
+
+# django model form mutation
+
+# drf model serializer mutation
+
+# bulk
+
+# small combination
+class Mutations(ObjectType):
+    category_create = CategoryCreate.Field()
